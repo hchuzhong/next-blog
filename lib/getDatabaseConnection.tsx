dede0934cb5c9364bd727f4dbbf319ab0437fc18
chuzhong <352995096@qq.com>
2021-09-20 23:58:17 +0800

    删除测试代码

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -3,10 +3,8 @@ import { createConnection, getConnectionManager } from "typeorm";
 const promise = (async function () {
   const manager = getConnectionManager();
   if (!manager.has("default")) {
-    console.log("创建 connection");
     return createConnection();
   } else {
-    console.log("复用 connection");
     const current = manager.get("default");
     if (current.isConnected) {
       return current;
