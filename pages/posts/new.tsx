f9b50efcecfd946f72666012db57256a40237655
chuzhong <352995096@qq.com>
2021-09-25 22:43:22 +0800

    封装 useForm 来代替 Form

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,73 +1,42 @@
-import { Form } from "components/Form";
 import { NextPage } from "next";
-import { useCallback, useState } from "react";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
+import { useForm } from "hooks/useForm";
 
 const PostsNew: NextPage = () => {
-  const [formData, setFormData] = useState({
-    title: "",
-    content: "",
-  });
-  const [errors, setErrors] = useState({
-    title: [] as string[],
-    content: [] as string[],
-  });
-  const onChange = useCallback(
-    (key, value) => {
-      setFormData({
-        ...formData,
-        [key]: value,
-      });
-    },
-    [formData]
-  );
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      axios.post(`/api/v1/posts`, formData).then(
-        () => {
-          window.alert("提交成功");
-        },
-        (error) => {
-          if (error.response) {
-            const response: AxiosResponse = error.response;
-            if (response.status === 422) {
-              setErrors(response.data);
-            }
+  const onSubmit = (formData: typeof initFormData) => {
+    axios.post(`/api/v1/posts`, formData).then(
+      () => {
+        window.alert("提交成功");
+      },
+      (error) => {
+        if (error.response) {
+          const response: AxiosResponse = error.response;
+          if (response.status === 422) {
+            setErrors(response.data);
           }
         }
-      );
-    },
-    [formData]
-  );
-  return (
-    <div>
-      <Form
-        fields={[
-          {
-            label: "标题",
-            type: "text",
-            value: formData.title,
-            onChange: (e) => onChange("title", e.target.value),
-            errors: errors.title,
-          },
-          {
-            label: "内容",
-            type: "textarea",
-            value: formData.content,
-            onChange: (e) => onChange("content", e.target.value),
-            errors: errors.content,
-          },
-        ]}
-        onSubmit={onSubmit}
-        buttons={
-          <>
-            <button type="submit">提交</button>
-          </>
-        }
-      />
-    </div>
-  );
+      }
+    );
+  };
+  const initFormData = { title: "", content: "" };
+  const { form, setErrors } = useForm({
+    initFormData,
+    fields: [
+      {
+        label: "标题",
+        type: "text",
+        key: "title",
+      },
+      {
+        label: "内容",
+        type: "textarea",
+        key: "content",
+      },
+    ],
+    buttons: <button type="submit">提交</button>,
+    onSubmit,
+  });
+  return <div>{form}</div>;
 };
 
 export default PostsNew;
