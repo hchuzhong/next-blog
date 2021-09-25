d709fae5c67ded02295302d87eec336fc321056d
chuzhong <352995096@qq.com>
2021-09-25 20:16:57 +0800

    完善 new 页面

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,7 +1,73 @@
+import { Form } from "components/Form";
 import { NextPage } from "next";
+import { useCallback, useState } from "react";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 const PostsNew: NextPage = () => {
-  return <div>Posts New</div>;
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+  });
+  const [errors, setErrors] = useState({
+    title: [] as string[],
+    content: [] as string[],
+  });
+  const onChange = useCallback(
+    (key, value) => {
+      setFormData({
+        ...formData,
+        [key]: value,
+      });
+    },
+    [formData]
+  );
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      axios.post(`/api/v1/posts`, formData).then(
+        () => {
+          window.alert("提交成功");
+        },
+        (error) => {
+          if (error.response) {
+            const response: AxiosResponse = error.response;
+            if (response.status === 422) {
+              setErrors(response.data);
+            }
+          }
+        }
+      );
+    },
+    [formData]
+  );
+  return (
+    <div>
+      <Form
+        fields={[
+          {
+            label: "标题",
+            type: "text",
+            value: formData.title,
+            onChange: (e) => onChange("title", e.target.value),
+            errors: errors.title,
+          },
+          {
+            label: "内容",
+            type: "textarea",
+            value: formData.content,
+            onChange: (e) => onChange("content", e.target.value),
+            errors: errors.content,
+          },
+        ]}
+        onSubmit={onSubmit}
+        buttons={
+          <>
+            <button type="submit">提交</button>
+          </>
+        }
+      />
+    </div>
+  );
 };
 
 export default PostsNew;
