355d999456a178e9b3002c93233e743c3c408e38
chuzhong <352995096@qq.com>
2021-10-10 17:12:53 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,8 @@ cd /home/blog/app &&
 git pull &&
 yarn install --production=false &&
 yarn build &&
+docker kill app &&
+docker rm app &&
 docker build -t hcz/node-web-app . &&
 docker run --name app --network=host -p 3000:3000 -d hcz/node-web-app
 echo 'OK!'
