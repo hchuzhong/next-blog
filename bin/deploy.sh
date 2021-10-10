0b157953daf5163e309849c4a063285a6f34f775
chuzhong <352995096@qq.com>
2021-10-10 17:01:08 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,9 @@
-echo hello > /tmp/frank
+docker start 111 &&
+cd /home/blog/app &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker kill app &&
+docker build -t hcz/node-web-app . &&
+docker run --name app --network=host -p 3000:3000 -d hcz/node-web-app
+echo 'OK!'
