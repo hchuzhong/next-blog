b5605e1cb787bdbf7de962d26a05f29ba84402ff
chuzhong <352995096@qq.com>
2021-10-10 11:53:35 +0800

    update ormconfig

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -9,6 +9,11 @@ const create = async () => {
   // @ts-ignore
   return createConnection({
     ...config,
+    host: process.env.NODE_ENV === "production" ? "localhost" : config.host,
+    database:
+      process.env.NODE_ENV === "production"
+        ? "blog_production"
+        : "blog_development",
     entities: [Post, User, Comment],
   });
 };
